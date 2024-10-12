a10847c97d901f6d472303f267daf327361fc0d0
redshiftstudio <accounts@rdshft.io>
2024-10-12 13:15:05 +0100

    refactor

--- main.c
+++ main.c
@@ -7,44 +7,40 @@ void swap(int *array, int index1, int index2)
     array[index2] = temp;
 }
 
-void printf_array(int array[], int stack_length)
+void printf_array(int arr[], int arr_size)
 {
-    for (int i = 0; i < stack_length; i++)
+    for (int i = 0; i < arr_size; i++)
     {
-        printf("%d, ", array[i]);
+        printf("%d, ", arr[i]);
     }
 }
 
 int steps_to_sort = 0;
-void test_and_sort(int arr[], int array_size)
+void test_and_sort(int arr[], int arr_size)
 {
-    for (int i = 0; i < array_size - 1; i++)
+    for (int i = 0; i < arr_size - 1; i++)
     {
         if (arr[i] > arr[i + 1])
         {
-            printf_array(arr, array_size);
+            printf_array(arr, arr_size);
             printf("Not sorted, continue...\n");
-            printf("\n");
             steps_to_sort++;
             swap(arr, i, i + 1);
-            test_and_sort(arr, array_size);
-
-            break;
+            test_and_sort(arr, arr_size);
         }
-        else if (i == array_size - 2)
+        else if (i == arr_size - 2)
         {
-            printf_array(arr, array_size);
+            printf_array(arr, arr_size);
             printf("Sorted! It took %d steps\n", steps_to_sort);
-            break;
         }
     }
 }
 
 int main()
 {
-    int stack[] = {96, 106, 28, 6, 0, 3, 5};
-    int stack_length = sizeof(stack) / sizeof(stack[0]);
-    test_and_sort(stack, stack_length);
+    int arr[] = {96, 106, 28, 6, 0, 3, 5};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    test_and_sort(arr, arr_size);
 
     return (0);
 }
